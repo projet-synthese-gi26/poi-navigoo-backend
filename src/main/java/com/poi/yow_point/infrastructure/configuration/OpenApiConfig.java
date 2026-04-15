2c1751a68662bb4f5df8d41b1f65d0ef1155fbc8
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2026-04-15 21:48:12 +0100

    Revise OpenAPI configuration for YowYob POI API
    Updated OpenAPI configuration with new API details and security scheme.

--- src/main/java/com/poi/yow_point/infrastructure/configuration/OpenApiConfig.java
+++ src/main/java/com/poi/yow_point/infrastructure/configuration/OpenApiConfig.java
@@ -1,31 +1,40 @@
 package com.poi.yow_point.infrastructure.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        Server prodServer = new Server();
-        prodServer.setUrl("https://poi-navigoo.pynfi.com");
-        prodServer.setDescription("Production server");
+  @Bean
+  public OpenAPI customOpenAPI() {
+    final String securitySchemeName = "bearerAuth";
 
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080");
-        localServer.setDescription("Local server");
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("POI Navigoo API")
-                        .version("1.0")
-                        .description("API for Point of Interest navigation services."))
-                .servers(List.of(prodServer, localServer));
-    }
+    return new OpenAPI()
+            .info(new Info()
+                    .title("YowYob POI Navigoo - API")
+                    .version("1.0.0")
+                    .description("Documentation de l'API POI Navigoo.")
+                    .contact(new Contact()
+                            .name("Équipe Backend")
+                            .email("dev@yowyob.com"))
+                    .license(new License()
+                            .name("Apache 2.0")
+                            .url("http://springdoc.org")))
+            .components(new Components()
+                    .addSecuritySchemes(securitySchemeName,
+                            new SecurityScheme()
+                                    .name(securitySchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+  }
 }
